24e8b7c238e7111294a04cf4153a19b6adc21994
Eteil Djoumatchoua (etdj) <etdj@odoo.com>
2022-12-28 17:02:00 +0100

    + test: createProduct, getProducts

--- product-service/src/main/java/com/djoumatch/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/djoumatch/productservice/controller/ProductController.java
@@ -2,11 +2,9 @@ package com.djoumatch.productservice.controller;
 
 import com.djoumatch.productservice.dto.ProductRequest;
 import com.djoumatch.productservice.dto.ProductResponse;
-import com.djoumatch.productservice.model.Product;
 import com.djoumatch.productservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,18 +25,10 @@ public class ProductController {
         productService.createProduct(productRequest);
     }
 
-    @PutMapping
-    public void updateProduct(@RequestBody Product product){}
-
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<ProductResponse> getAllProducts(){
         return productService.getAllProducts();
     }
 
-    @GetMapping("/{id}")
-    public void getProductById(@PathVariable String id){}
-
-    @DeleteMapping("/{id}")
-    public void deleteProduct(@PathVariable String id){}
 }
--- product-service/src/test/java/com/djoumatch/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/djoumatch/productservice/ProductServiceApplicationTests.java
@@ -1,19 +1,43 @@
 package com.djoumatch.productservice;
 
+import com.djoumatch.productservice.dto.ProductRequest;
+import com.djoumatch.productservice.repository.ProductRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.math.BigDecimal;
 
 @SpringBootTest
 @Testcontainers
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
     // Create a docker container for the tests
     @Container
     static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
+
+    // Used to create mock tests
+    @Autowired
+    private MockMvc mockMvc;
+
+    // Used to map object to  string
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private ProductRepository productRepository;
 
     // Dynamically set the database uri by giving the container uri
     @DynamicPropertySource
@@ -22,7 +46,38 @@ class ProductServiceApplicationTests {
     }
 
     @Test
-    void contextLoads() {
+    void shouldCreateProduct() throws Exception {
+        ProductRequest productRequest = getProductRequest();
+        String productRequestString = objectMapper.writeValueAsString(productRequest);
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(productRequestString))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+
+        Assertions.assertEquals(1, productRepository.findAll().size());
+
+    }
+
+    @Test
+    void ShouldGetProducts() throws Exception {
+        int nbProducts = productRepository.findAll().size();
+        // insert one product
+        ProductRequest productRequest = getProductRequest();
+        String productRequestString = objectMapper.writeValueAsString(productRequest);
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(productRequestString))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+
+        Assertions.assertEquals(nbProducts + 1, productRepository.findAll().size());
+    }
+
+    private ProductRequest getProductRequest() {
+        return ProductRequest.builder()
+                .name("iphone13")
+                .description("iphone")
+                .price(BigDecimal.valueOf(1200))
+                .build();
     }
 
 }
