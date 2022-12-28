22b8e398b538d20ab13f2987c968ad5a4feda1e0
Eteil Djoumatchoua (etdj) <etdj@odoo.com>
2022-12-28 23:07:05 +0100

    wrap all services together

--- inventory-service/src/main/java/com/djoumatch/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/djoumatch/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,11 @@
 package com.djoumatch.inventoryservice;
 
+import com.djoumatch.inventoryservice.model.Inventory;
+import com.djoumatch.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -10,4 +14,21 @@ public class InventoryServiceApplication {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
 
+
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository){
+		return args -> {
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("i13");
+			inventory.setQuantity(150);
+
+			Inventory inventory2 = new Inventory();
+			inventory2.setSkuCode("i13_pro");
+			inventory2.setQuantity(15);
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory2);
+
+		};
+	}
 }
