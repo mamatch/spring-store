e71b75aa43170a5896c0bfef8e770ece83ebb184
Eteil Djoumatchoua (etdj) <etdj@odoo.com>
2022-12-28 16:14:51 +0100

    + test containers configurations

--- product-service/src/test/java/com/djoumatch/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/djoumatch/productservice/ProductServiceApplicationTests.java
@@ -2,9 +2,24 @@ package com.djoumatch.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class ProductServiceApplicationTests {
+    // Create a docker container for the tests
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
+
+    // Dynamically set the database uri by giving the container uri
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry){
+        dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
 
     @Test
     void contextLoads() {
